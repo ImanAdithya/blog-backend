83c8c00cb829eac7fe5bdb9b4bacf0efda040bf9
adithya <adithyaiman07@gmail.com>
2025-07-28 22:49:38 +0530

    Set all http methods wrap in jwt

--- src/blog/controller/blog.controller.ts
+++ src/blog/controller/blog.controller.ts
@@ -21,22 +21,25 @@ export class BlogController {
   }
 
   @Get(':id')
+  @UseGuards(JwtAuthGuard)
   findOne(@Param('id') id: number) {
     return this.blogService.findOne(id);
   }
 
   @Get('user/:userId')
+  @UseGuards(JwtAuthGuard)
     getPostsByUser(@Param('userId') userId: number) {
     return this.blogService.findByUserId(userId);
   }
 
-
   @Put(':id')
+  @UseGuards(JwtAuthGuard)
   update(@Param('id') id: number, @Body() dto: UpdateBlogDto) {
     return this.blogService.update(id, dto);
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   delete(@Param('id') id: number) {
     return this.blogService.delete(id);
   }
