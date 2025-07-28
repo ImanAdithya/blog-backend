4af6863c1d11a11134f7fe27fa1f39b818bc187d
adithya <adithyaiman07@gmail.com>
2025-07-28 21:14:55 +0530

    Added blog service crud oprations

--- src/blog/service/blog.service.ts
+++ src/blog/service/blog.service.ts
@@ -1,12 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Blog } from 'src/entity/Blog';
+import { User } from 'src/entity/User';
 import { Repository } from 'typeorm';
+import { CreateBlogDto } from '../dto/create-blog.dto';
+import { UpdateBlogDto } from '../dto/update-blog.dto';
 
 @Injectable()
 export class BlogService {
-     constructor(@InjectRepository(Blog) private userRepository : Repository<Blog>){
-        
-     }
-    
+    constructor(
+        @InjectRepository(Blog) private blogRepository: Repository<Blog>,
+        @InjectRepository(User) private userRepository: Repository<User>
+  ) {}
+
+  async create(dto: CreateBlogDto) {
+    const user = await this.userRepository.findOne({ where: { id: dto.userId } });
+    if (!user) throw new NotFoundException('User not found');
+
+    const blog = this.blogRepository.create({
+      title: dto.title,
+      description: dto.description,
+      user,
+    });
+
+    await this.blogRepository.save(blog);
+
+    return { code: 200, message: 'Blog created', data: blog };
+  }
+
+  async findAll() {
+    const blogs = await this.blogRepository.find();
+    return { code: 200, message: 'Success', data: blogs };
+  }
+
+  async findOne(id: number) {
+    const blog = await this.blogRepository.findOne({ where: { id } });
+    if (!blog) throw new NotFoundException('Blog not found');
+    return { code: 200, message: 'Success', data: blog };
+  }
+
+  async update(id: number, dto: UpdateBlogDto) {
+    const blog = await this.blogRepository.findOne({ where: { id } });
+    if (!blog) throw new NotFoundException('Blog not found');
+
+    blog.title = dto.title ?? blog.title;
+    blog.description = dto.description ?? blog.description;
+
+    await this.blogRepository.save(blog);
+    return { code: 200, message: 'Updated successfully', data: blog };
+  }
+
+  async delete(id: number) {
+    const blog = await this.blogRepository.findOne({ where: { id } });
+    if (!blog) throw new NotFoundException('Blog not found');
+    await this.blogRepository.remove(blog);
+    return { code: 200, message: 'Deleted successfully', data: null };
+  }
+
 }
