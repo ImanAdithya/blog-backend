246bd7db1994322036cd4c5180be5bb460b43e4a
adithya <adithyaiman07@gmail.com>
2025-07-28 20:51:22 +0530

    Added user and blog orm mapping

---
+++ src/entity/Blog.ts
@@ -0,0 +1,18 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { User } from './User';
+
+
+@Entity({ name: 'blog' })
+export class Blog {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column('text')
+  description: string;
+
+  @ManyToOne(() => User, user => user.blogs, { eager: true, onDelete: 'CASCADE' })
+  user: User;
+}
