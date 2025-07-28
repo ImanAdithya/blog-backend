39167f6e0f7fc908db5669c45685742c7f710bfa
adithya <adithyaiman07@gmail.com>
2025-07-28 20:44:57 +0530

    Added User Create

--- src/user/controller/user.controller.ts
+++ src/user/controller/user.controller.ts
@@ -1,7 +1,14 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from '../service/user.service';
+import { UserDto } from '../dto/User.dto';
 
 @Controller('user')
 export class UserController {
-    constructor(){}
+    constructor(private userservice:UserService){}
+
+    @Post()
+    createUser(@Body() userDto: UserDto) {
+        return this.userservice.createUser(userDto);
+    } 
 
 }
--- src/user/service/user.service.ts
+++ src/user/service/user.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/entity/User';
+import { UserParams } from 'src/util/type';
 import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
     constructor(@InjectRepository(User) private userRepository : Repository<User>){}
 
-    createUser(){}
+    createUser(user:UserParams){
+        this.userRepository.create(user);
+    }
 }
