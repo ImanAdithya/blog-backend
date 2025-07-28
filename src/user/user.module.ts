e5ab916d9324a77b3238afecdf02fc86b559fafa
adithya <adithyaiman07@gmail.com>
2025-07-28 20:32:14 +0530

    Re correct structure of suer Module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { UserController } from './controller/user/user.controller';
-import { UserService } from './service/user/user.service';
+import { UserController } from './controller/user.controller';
+import { UserService } from './service/user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entity/User';
 
